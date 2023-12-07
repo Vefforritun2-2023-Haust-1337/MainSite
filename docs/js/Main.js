99bf7cde71d0fa510b1bbe4a0eaf6b51dd96b149
kartoflu-supa <serik.olafur@gmail.com>
2023-12-07 10:02:05 +0000

    specialList

--- docs/js/Main.js
+++ docs/js/Main.js
@@ -1,6 +1,43 @@
 "use strict"
 
-import { creoDiv, creoHref, regoElem } from "./Functions"
+//import { creoDiv, creoHref, regoElem } from "./Functions"
+function creoDiv(klas = "",text = "")
+{
+    let elem;
+    elem = document.createElement("div");
+    if (!(klas == ""))
+    {
+        elem.className = klas;
+    }
+    if (!(text == ""))
+    {
+        elem.textContent = text;
+    }
+    return elem;
+}
+
+function creoHref(klas = "",text = "",href = "")
+{
+    let elem;
+    elem = document.createElement("a");
+    elem.href = href;
+    if (!(klas == ""))
+    {
+        elem.className = klas;
+    }
+    if (!(text == ""))
+    {
+        elem.textContent = text;
+    }
+    return elem;
+}
+
+function regoElem(children = [], paren = creoDiv())
+{
+    children.forEach(child =>{
+        paren.appendChild(child);
+    })
+}
 
 function creoNavBar()
 {
@@ -12,4 +49,57 @@ function creoNavBar()
         creoHref("navbutton","Verkefni","https://vefforritun2-2023-haust-1337.github.io/MainSite/MainIndex.html")
     ]
     regoElem(hrefs,navBarDiv);
+}
+
+async function loadJson(file = "/data.json")
+{
+    let response = await fetch(file);
+    if (response.status === 200)
+    {
+        console.log(response.status,"Json Status");
+        console.log(response.statusText);
+        return await response.json();
+    }
+    else
+    {
+        console.log(response.status,"Json Status");
+        console.log(response.statusText);
+    }
+}
+
+function startContent(data,key)
+{
+    let entries = [];
+    data[key].forEach(elem =>{
+        let entry = creoDiv("entry")
+        let contentEntry = [
+            creoDiv("id",elem["id"]),
+            creoDiv("name",elem["name"]),
+            creoDiv("specialty",elem["specialty"]),
+            creoDiv("location",elem["location"]),
+            creoDiv("email",elem["contact"]["email"]),
+            creoDiv("phone",elem["contact"]["phone"])
+        ];
+        regoElem(contentEntry,entry);
+        entries.push({
+            data:elem,
+            element:entry
+        });
+    })
+    return entries;
+}
+
+async function init(data = "Mainsite/docs/data/Serfaedinga.json")
+{
+    let jsonData = await loadJson(data);
+    let list = creoDiv("list");
+    let entries = startContent(jsonData,"specialists");
+    regoElem (list,entries);
+    specialList.appendChild(list);
+}
+
+let specialList = document.querySelector("#Specialists");
+if (typeof specialList != "undefined")
+{
+    init();
 }
