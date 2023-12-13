7ff82c6baf55452dc6e451b8cad07601be1c1729
kartoflu-supa <serik.olafur@gmail.com>
2023-12-13 16:21:50 +0000

    firebase part 2

--- docs/js/Main.js
+++ docs/js/Main.js
@@ -4,7 +4,7 @@
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-  import { getDatabase, ref, child, get , onValue} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+  import { getDatabase, ref, child, get, set} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
 
@@ -47,7 +47,39 @@ const specialRegi = document.querySelector("#Register");
 const navbar = document.querySelector("#Navbar");
 const taskList = document.querySelector("#Tasks");
 const leaderboard = document.querySelector("#Board");
+const specialForm = document.querySelector("#specialform");
 
+
+function creoElem(klas = "",text = "",imag = "",type = "",href = ""){
+    let elem;
+    if (!(type === ""))
+    {
+        elem = document.createElement("input");
+        elem.type = type;
+    }
+    else if (!(href === ""))
+    {
+        elem = document.createElement("a");
+        elem.href = href;
+    }
+    else 
+    {
+        elem = document.createElement("div");
+    }
+    if (!(klas === "")){
+        elem.className = klas
+    }
+    if (!(text === "")){
+        elem.textContent = text;
+    }
+    if (!(imag === "")){
+        let photo = [document.createElement("img")];
+        photo[0].src = imag;
+        regoElem(photo,elem);
+    }
+    return elem
+}
+/*----------unused---------------
 function creoDiv(klas = "",text = "")
 {
     let elem;
@@ -77,7 +109,7 @@ function creoHref(klas = "",text = "",href = "")
     }
     return elem;
 }
-
+---------------------------------*/
 function regoElem(children = [], paren)
 {
     children.forEach(child =>{
@@ -87,17 +119,18 @@ function regoElem(children = [], paren)
 
 function creoNavBar()
 {
-    let navBarDiv = creoDiv("topnav");
+    let navBarDiv = creoElem("topnav");
     let hrefs = [
-        creoHref("icon-home3","Heim","./MainIndex.html"),
-        creoHref("icon-info","Um Okkur","./aboutMe.html"),
-        creoHref("icon-user-tie","Sérfræðingar","./specialists.html"),
-        creoHref("icon-bubble","Verkefni","https://vefforritun2-2023-haust-1337.github.io/MainSite/MainIndex.html")
+        creoElem("icon-home3","Heim","","","./MainIndex.html"),
+        creoElem("icon-info","Um Okkur","","","./aboutMe.html"),
+        creoElem("icon-user-tie","Sérfræðingar","","","./specialists.html"),
+        creoElem("icon-bubble","Verkefni","","","https://vefforritun2-2023-haust-1337.github.io/MainSite/MainIndex.html")
     ]
     regoElem(hrefs,navBarDiv);
     navbar.appendChild(navBarDiv);
 }
 
+/*----------unused---------------
 async function loadJson(file = "/data.json")
 {
     let response = await fetch(file);
@@ -113,21 +146,19 @@ async function loadJson(file = "/data.json")
         console.log(response.statusText);
     }
 }
-
-function startContent(data)
+---------------------------------*/
+function startContent(data,dataset)
 {
     let entries = [];
-    console.log(data);
     data.forEach(elem =>{
-        console.log(elem);
-        let entry = creoDiv("entry")
+        let entry = creoElem("entry")
         let contentEntry = [
-            creoDiv("id",elem["id"]),
-            creoDiv("name",elem["name"]),
-            creoDiv("specialty",elem["specialty"]),
-            creoDiv("location",elem["location"]),
-            creoDiv("email",elem["contact"]["email"]),
-            creoDiv("phone",elem["contact"]["phone"])
+            /*creoElem("id",elem["id"]),*/
+            creoElem("name",elem["name"]),
+            creoElem("specialty",elem["specialty"]),
+            creoElem("location",elem["location"]),
+            creoElem("email",elem["contact"]["email"]),
+            creoElem("phone",elem["contact"]["phone"])
         ];
         regoElem(contentEntry,entry);
         entries.push({
@@ -138,36 +169,62 @@ function startContent(data)
     return entries;
 }
 
-async function creoDatalist(zone = creoDiv(),dataset = "specialists")
+async function creoDatalist(zone = creoElem(),dataset = "specialists")
 {   
-    let jsonData = [];
     const dbRef = ref(getDatabase());
     get(child(dbRef, `${dataset}`)).then((snapshot) => {
       if (snapshot.exists()) {
+        let jsonData = [];
         console.log("data read");
         snapshot.val().forEach(element => {
             jsonData.push(element);
         });
-      } else {
+        let entries = startContent(jsonData,dataset);
+        let list = creoElem("col-3");
+        entries.forEach(elem =>{
+            list.appendChild(elem["element"])
+        })
+        zone.appendChild(list);
+      } 
+      else {
         console.log("No data available");
       }
     }).catch((error) => {
       console.error(error);
     });
-    let list = creoDiv("list");
-    let entries = startContent(jsonData,dataset);
-    entries.forEach(elem =>{
-        list.appendChild(elem["element"])
-    })
-    zone.appendChild(list);
 }
 
 function register()
 {
     let elems = [
-        creoDiv("paragrah","Register as a specialist here."),
-        creoHref("button","Register","./specialistsForm.html")];
+        creoElem("paragrah","Register as a specialist here."),
+        creoElem("button","Register","./specialistsForm.html")];
     regoElem(elems,specialRegi);
+}
+
+function writeData(dataset = "specialists",data = {})
+{
+    const db = getDatabase();
+    set(ref(db, `${dataset}`), {
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  
+}
+
+function creoRegiform(zone = creoElem(),dataset = "specialists")
+{
+    let elem = creoElem("userinput");
+    if (dataset = "specialists")
+    {
+
+    }
+    else if (dataset = "contracts")
+    {
+
+    }
+    zone.appendChild(elem);
 }
 
 if (navbar)
@@ -188,4 +245,9 @@ if(specialRegi)
 if (leaderboard)
 {
     creoDatalist(leaderboard,"contracts");
+}
+
+if (specialForm)
+{
+    creoRegiform(specialForm);
 }
